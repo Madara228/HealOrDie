2a9e561a4ae2a15d216f27f35aadfaee8b2a4eb2
Nekot94 <nekomato9@gmail.com>
2017-07-14 13:42:10 +0300

    heal

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,21 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float tilt;
-
+    public Boundary boundary;
     private Rigidbody myBody;
    
 
 	void Start ()
     {
         myBody = GetComponent<Rigidbody>();
+
 	}
 
-    void Update()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         myBody.velocity = movement * speed;
+        myBody.position = new Vector3(
+            Mathf.Clamp(transform.position.x, boundary.minX, boundary.maxX),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, boundary.minZ, boundary.maxZ)
+            );
+            
         myBody.rotation = Quaternion.Euler(0, 0, -tilt * myBody.velocity.x);
     }
 
