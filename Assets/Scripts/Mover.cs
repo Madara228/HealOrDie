a35abe4de99865391423e3646f4c30c9507f9afb
Nekot94 <nekomato9@gmail.com>
2017-07-14 14:10:26 +0300

    heal2

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody myBody;
+
+	void Start ()
+    {
+        myBody = GetComponent<Rigidbody>();
+        myBody.velocity = transform.forward * speed;
+	}
+	
+}
