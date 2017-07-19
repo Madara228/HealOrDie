4fba6a7407b1d71659086657ddca94b01b2275a1
Nekot94 <nekomato9@gmail.com>
2017-07-19 14:02:22 +0300

    bratishka

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject spawnObject;
+    public Vector3 spawnValues;
+
+	// Use this for initialization
+	void Start () {
+        SpawnWaves();
+	}
+	
+	void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-spawnValues.x, spawnValues.x),
+            spawnValues.y,
+            spawnValues.z);
+        Instantiate(spawnObject, spawnPosition, Quaternion.identity);
+
+    }
+}
---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody myBody;
+
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        myBody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+
+    //   void Update () {
+    //       transform.Rotate(10, 15, 15);
+    //}
+}
