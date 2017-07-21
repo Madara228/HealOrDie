86ecddee2b19cd2fc71d3f82b7829bbfbe49eedb
Nekot94 <nekomato9@gmail.com>
2017-07-21 12:45:15 +0300

    amin

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public int spawnCount;
     public GameObject spawnObject;
     public Vector3 spawnValues;
 
-	// Use this for initialization
-	void Start () {
-        SpawnWaves();
-	}
-	
-	void SpawnWaves()
+    void Start()
     {
-        Vector3 spawnPosition = new Vector3(
-            Random.Range(-spawnValues.x, spawnValues.x),
-            spawnValues.y,
-            spawnValues.z);
-        Instantiate(spawnObject, spawnPosition, Quaternion.identity);
+       
+        StartCoroutine(SpawnWaves());
+    }
 
+	IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(
+                    Random.Range(-spawnValues.x, spawnValues.x),
+                    spawnValues.y,
+                    spawnValues.z);
+                Instantiate(spawnObject, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
